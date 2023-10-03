96872636c9ee6b2dbf478b22a27a3802a952df63
Ethan Rogers <ethan.rogers@daveramsey.com>
2023-10-03 11:14:50 -0500

    don't use fancy gbl at this time...

--- zsh/functions/git.sh
+++ zsh/functions/git.sh
@@ -1,22 +1,3 @@
-gbl() {
-  local branches
-  branches=$(git branch --list | sed 's/^\*\?\s*//')  # Get the list of branches, remove leading '*' from the current branch
-
-  # Use dialog to create a menu
-  local branch
-  branch=$(dialog --clear --title "Git Branch Selector" --menu "Select a branch to checkout:" 0 0 0 "${branches[@]}" 2>&1 >/dev/tty)
-
-  # Check if the user pressed Enter or Esc
-  local exit_code=$?
-  if [ $exit_code -eq 0 ]; then
-    # Checkout the selected branch
-    git checkout "$branch"
-    echo "Checked out branch: $branch"
-  else
-    echo "Operation canceled."
-  fi
-}
-
 gbs() {
     BRANCH=$(git branch | cat | cut -c 3- | grep -iF "$1")
     if [[ -n "${BRANCH}" ]]
