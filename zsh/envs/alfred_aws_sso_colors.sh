b507ebfd65cf391327510547462e36cacec7f70e
Ethan Rogers <ethan.rogers@daveramsey.com>
2023-10-19 08:35:52 -0500

    [alfred] add envs for colors.py for aws sso config

---
+++ zsh/envs/alfred_aws_sso_colors.sh
@@ -0,0 +1,8 @@
+export COLOR_TEST=green
+export COLOR_QA=yellow
+export COLOR_PROD=red
+export COLOR_OTHER=orange
+export ICON_TEST=circle
+export ICON_QA=circle
+export ICON_PROD=circle
+export ICON_OTHER=circle
