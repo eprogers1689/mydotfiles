c0cf0c0bfeae20b7afe36a315d2ac7316bb29d1a
Ethan Rogers <ethan.rogers@daveramsey.com>
2023-10-26 09:26:18 -0500

    adjust pl to use ENVS and add plo to unset

--- zsh/aliases/pulumi.sh
+++ zsh/aliases/pulumi.sh
@@ -1,4 +1,3 @@
 alias p='pulumi'
-alias pl='pulumi login s3://rs-pulumi-state-$(aws sts get-caller-identity --query 'Account' --output text)'
 alias pup='pulumi up'
 alias puppy='pulumi up --yes'
--- zsh/functions/pulumi.sh
+++ zsh/functions/pulumi.sh
@@ -9,3 +9,20 @@ showstate(){
     echo $STATE_BUCKET
     aws s3 cp "${STATE_BUCKET}/.pulumi/stacks/${STACK_NAME}.json" - | code -
 }
+
+pl(){
+    AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+    PULUMI_BACKEND_URL="s3://rs-pulumi-state-$AWS_ACCOUNT_ID"
+    matched_account=$(jq -r ".accountList | sort_by(.accountName)[] | [.accountId,.accountName] | @tsv" ~/.aws/sso_accounts.json | grep $AWS_ACCOUNT_ID | awk '{print $2}' )
+    echo "setting PULUMI_BACKEND_URL to $PULUMI_BACKEND_URL \e[90m($matched_account)\e[0m"
+    export PULUMI_BACKEND_URL=$PULUMI_BACKEND_URL
+}
+
+plo(){
+  if [[ ! -z "$PULUMI_BACKEND_URL" ]]; then
+    echo "Logging out of: $PULUMI_BACKEND_URL"
+    unset PULUMI_BACKEND_URL
+  else
+    echo "Already logged out of pulumi..."
+  fi
+}
