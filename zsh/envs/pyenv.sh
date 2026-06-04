0b24037a8085c84ab3f245683862c1ae13c032d6
Ethan Rogers <ethan.rogers@daveramsey.com>
2026-06-04 07:18:47 -0500

    fix pyenv sucks

--- zsh/envs/pyenv.sh
+++ zsh/envs/pyenv.sh
@@ -1,10 +1,8 @@
 # pyenv stuff
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
-eval "$(pyenv init -)"
-
 if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init --path)"
   eval "$(pyenv init -)"
 fi
 
